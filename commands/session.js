bed4888fb725fac15eaed8d1cd3b5e8985f2ec98
Lisa <koller.lisa@gmx.net>
2021-02-27 17:41:27 +0100

    feat: #2 async and #3 some switch-statements removed and #7 partials

--- commands/session.js
+++ commands/session.js
@@ -22,20 +22,25 @@ function getNextMonday() {
 }
 
 const emojiList = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹', '🇺', '🇻', '🇼', '🇽', '🇾', '🇿']
-let options = []
-for(let i = 0; i < nDays; i++) {
-  nextMonday.add(i === 0 ? 0 : 1, 'days')
-  let weekday = nextMonday.format('dddd')
-  let dateAsNumber = nextMonday.date()
-  let icon = emojiList[i]
-  options.push(
-    {
-      'icon': icon,
-      'text': `${icon}  ${weekday} (${dateAsNumber}.)`,
-      'weekday': weekday,
-      'value': '-'
-    }
-  )
+let options = buildOptions(nDays, nextMonday)
+
+function buildOptions(max, startingOn) {
+  let result = []
+  for(let i = 0; i < max; i++) {
+    startingOn.add(i === 0 ? 0 : 1, 'days')
+    let weekday = startingOn.format('dddd')
+    let dateAsNumber = startingOn.date()
+    let icon = emojiList[i]
+    result.push(
+      {
+        'icon': icon,
+        'text': `${icon}  ${weekday} (${dateAsNumber}.)`,
+        'weekday': weekday,
+        'value': '-'
+      }
+    )
+  }
+  return result
 }
 
 function _getBasicEmbedFields() {
@@ -168,11 +173,59 @@ function getTopAnswer(sentMessage) {
   return result.length > 0 ? result : '-'
 }
 
+async function updateFields(receivedEmbed, sentMessage, reaction, user, type) {
+  return new Promise((resolve, reject) => {
+    if(!(type === 'add' || type === 'remove')) {
+      reject('Wrong type!')
+    }
+    let result = receivedEmbed.fields.map((entry, index) => {
+      let o = Object.assign({}, entry)
+
+      if(index === 0) {
+        o.value = getTopAnswer(sentMessage)
+      } else if(index >= 2 && reaction.emoji.name === options[index-2]['icon']) {
+        // index-2 because there are 2 "standard" fields that need to be subtracted (intro + blank space; see basicEmbedFieldsA)
+        o.value = type === 'add' ? addUser(o.value, user) : removeUser(o.value, user)
+      }
+
+      return o
+    })
+    resolve(result)
+  })
+}
+
 module.exports = {
   name: 'session',
   description: 'Wann soll die nächste Session stattfinden?',
   args: false,
-  execute(message, args) {
+  async handleReaction(message, reaction, user, type) {
+    try {
+      console.log('--- hi there, I am the function to add')
+      if(options.some(option => option.icon === reaction.emoji.name)) {
+        const receivedEmbed = await message.embeds[0]
+
+        let fields = await updateFields(receivedEmbed, message, reaction, user, type)
+
+        const updatedEmbed = {
+          color: receivedEmbed.color,
+          title: receivedEmbed.title,
+          author: receivedEmbed.author,
+          description: receivedEmbed.description,
+          thumbnail: receivedEmbed.thumbnail,
+          fields: fields,
+          timestamp: receivedEmbed.timestamp,
+          footer: receivedEmbed.footer,
+        }
+
+        message.edit({ embed: updatedEmbed })
+      } else {
+        console.log('Wrong reaction dude 🙄')
+      }
+    } catch(error) {
+      console.log('Something went wrong: ', error)
+    }
+  },
+  async execute(message, args) {
     /*if (!args.length) {
       return message.channel.send(
         `You didn't provide any arguments, ${message.author}!`
@@ -184,136 +237,19 @@ module.exports = {
     embed.author.name = message.author['username']
     embed.author.icon_url = message.author.avatarURL()
 
-    message.channel
-      .send({ /*files: [file], */ embed: embed })
-      .then((sentMessage) => {
-        sentMessage
-          .react(options[0]['icon'])
-          .then(() => sentMessage.react(options[1]['icon']))
-          .then(() => sentMessage.react(options[2]['icon']))
-          .then(() => sentMessage.react(options[3]['icon']))
-          .then(() => sentMessage.react(options[4]['icon']))
-          .then(() => sentMessage.react(options[5]['icon']))
-          .then(() => sentMessage.react(options[6]['icon']))
-          .catch(() => console.error('One of the emojis failed to react.'))
-
-        const filter = (reaction, user) => {
-          return (
-            user.username !== 'Date-Selection-Bot' &&
-            options.some(option => option.icon === reaction.emoji.name)
-          )
-        }
-
-        const collector = sentMessage.createReactionCollector(filter, {
-          dispose: true,
-        })
-
-        collector.on('collect', (reaction, user) => {
-          console.log(`Collected ${reaction.emoji.name} from ${user.tag}`)
-
-          const receivedEmbed = sentMessage.embeds[0]
-
-          let fields = receivedEmbed.fields.map((entry, index) => {
-            let o = Object.assign({}, entry)
-
-            switch (true) {
-              case index === 0:
-                o.value = getTopAnswer(sentMessage)
-                break
-              case index === 2 && reaction.emoji.name === options[0]['icon']:
-                o.value = addUser(o.value, user)
-                break
-              case index === 3 && reaction.emoji.name === options[1]['icon']:
-                o.value = addUser(o.value, user)
-                break
-              case index === 4 && reaction.emoji.name === options[2]['icon']:
-                o.value = addUser(o.value, user)
-                break
-              case index === 5 && reaction.emoji.name === options[3]['icon']:
-                o.value = addUser(o.value, user)
-                break
-              case index === 6 && reaction.emoji.name === options[4]['icon']:
-                o.value = addUser(o.value, user)
-                break
-              case index === 7 && reaction.emoji.name === options[5]['icon']:
-                o.value = addUser(o.value, user)
-                break
-              case index === 8 && reaction.emoji.name === options[6]['icon']:
-                o.value = addUser(o.value, user)
-                break
-            }
-            return o
-          })
-
-          const updatedEmbed = {
-            color: receivedEmbed.color,
-            title: receivedEmbed.title,
-            author: receivedEmbed.author,
-            description: receivedEmbed.description,
-            thumbnail: receivedEmbed.thumbnail,
-            fields: fields,
-            timestamp: receivedEmbed.timestamp,
-            footer: receivedEmbed.footer,
-          }
-
-          sentMessage.edit({ embed: updatedEmbed })
-        })
-
-        collector.on('remove', (reaction, user) => {
-          console.log(`Remove ${reaction.emoji.name} from ${user.tag}`)
-
-          const receivedEmbed = sentMessage.embeds[0]
-
-          let fields = receivedEmbed.fields.map((entry, index) => {
-            let o = Object.assign({}, entry)
-
-            switch (true) {
-              case index === 0:
-                o.value = getTopAnswer(sentMessage)
-                break
-              case index === 2 && reaction.emoji.name === options[0]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-              case index === 3 && reaction.emoji.name === options[1]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-              case index === 4 && reaction.emoji.name === options[2]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-              case index === 5 && reaction.emoji.name === options[3]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-              case index === 6 && reaction.emoji.name === options[4]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-              case index === 7 && reaction.emoji.name === options[5]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-              case index === 8 && reaction.emoji.name === options[6]['icon']:
-                o.value = removeUser(o.value, user)
-                break
-            }
-            return o
-          })
-
-          const updatedEmbed = {
-            color: receivedEmbed.color,
-            title: receivedEmbed.title,
-            author: receivedEmbed.author,
-            description: receivedEmbed.description,
-            thumbnail: receivedEmbed.thumbnail,
-            fields: fields,
-            timestamp: receivedEmbed.timestamp,
-            footer: receivedEmbed.footer,
-          }
-
-          sentMessage.edit({ embed: updatedEmbed })
-        })
-
-        collector.on('end', (collected) => {
-          console.log(`Collected ${collected.size} items`)
-        })
-      })
+    try {
+      let sentMessage = await message.channel.send({ /*files: [file], */ embed: embed })
+      await sentMessage.react(options[0]['icon'])
+      await sentMessage.react(options[1]['icon'])
+      await sentMessage.react(options[2]['icon'])
+      await sentMessage.react(options[3]['icon'])
+      await sentMessage.react(options[4]['icon'])
+      await sentMessage.react(options[5]['icon'])
+      await sentMessage.react(options[6]['icon'])
+    } catch(error) {
+      console.error('One of the emojis failed to react.')
+    }
+    
     message.channel.send('@everyone Es gibt wieder was zum Abstimmen ⬆')
   },
 }
--- index.js
+++ index.js
@@ -1,22 +1,78 @@
-require('dotenv').config();
+require('dotenv').config()
 const fs = require('fs')
 const Discord = require('discord.js')
 const { prefix } = require('./config.json')
 const token = process.env.TOKEN
 
-const client = new Discord.Client()
+const client = new Discord.Client({
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+})
 client.commands = new Discord.Collection()
 
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter((file) => file.endsWith('.js'))
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
   client.commands.set(command.name, command)
 }
 
-client.on('message', (message) => {
+async function handleReactions(reaction, user, type) {
+  if (!(type === 'add' || type === 'remove')) {
+    console.log('Wrong type: ', type)
+    return
+  }
+  if (user.bot === true) return
+
+  let fullmessage
+
+  if (reaction.message.partial) {
+    console.log('Hi there, partial!')
+    try {
+      fullmessage = await reaction.message.fetch()
+    } catch (error) {
+      console.error('Something went wrong when fetching the message: ', error)
+    }
+  } else {
+    console.log('This is not a message partial')
+    fullmessage = reaction.message
+  }
+
+  const author = await fullmessage.author.fetch()
+  const title = fullmessage.embeds[0].title
+
+  if (author.bot === true && author.id === '802091512573722654' && title === 'Nächste Gaming-Session') {
+    console.log('I need to do something with those reactions!')
+
+    const command = client.commands.get('session')
+    try {
+      command.handleReaction(fullmessage, reaction, user, type)
+    } catch (error) {
+      console.error(error)
+      message.reply('Ein Fehler ist dabei aufgetreten das Kommando auszuführen! :(')
+    }
+  } else {
+    console.log('This is not a message where I care about the reactions.')
+  }
+
+  if (type === 'add') {
+    console.log(`${user.username} reacted with "${reaction.emoji.name}".`)
+  } else {
+    console.log(`${user.username} removed reaction "${reaction.emoji.name}".`)
+  }
+}
+
+// to handle reactions to messages
+client.on('messageReactionAdd', async (reaction, user) => {
+  handleReactions(reaction, user, 'add')
+})
+
+// to handle removal of reactions
+client.on('messageReactionRemove', async (reaction, user) => {
+  handleReactions(reaction, user, 'remove')
+})
+
+// to handle new messages in the chat
+client.on('message', async (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return
 
   const args = message.content.slice(prefix.length).trim().split(/ +/)
