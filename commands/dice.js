857d7fa06fa7aff2a387aa7d13642189303e0134
Lisa <koller.lisa@gmx.net>
2021-03-06 13:58:25 +0100

    feat: #18 dice command

---
+++ commands/dice.js
@@ -0,0 +1,25 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+module.exports = {
+  name: 'dice',
+  description: 'Wirf den virtuellen Würfel.',
+  args: false,
+  execute(message, args) {
+    const result = getRandomInt(1, 6)
+
+    message.channel.send(`${message.author.username} wirft den Würfel des Schicksals 🎲 ...`)
+    setTimeout(function () {
+      message.channel.send(`${message.author.username} beobachtet ihn ganz genau 👀 ...`)
+    }, 2000)
+    setTimeout(function () {
+      message.channel.send(`Und das Ergebnis ist...`)
+    }, 4000)
+    setTimeout(function () {
+      message.channel.send(`**${result}**! 🎉`)
+    }, 7000)
+  },
+}
